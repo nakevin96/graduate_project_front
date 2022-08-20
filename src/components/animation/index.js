a18c87c45c15d612ae3c97bd8f9168c222f55334
nakevin96 <nakevin96@hanyang.ac.kr>
2022-08-21 02:12:20 +0900

    loading animation add

--- src/components/animation/index.js
+++ src/components/animation/index.js
@@ -1,4 +1,5 @@
 import PersonAnimation from './person-animation/PersonAnimation';
 import MiningLoadingAnimation from './mining-loading-animation/MiningLoadingAnimation';
+import DetailLoadingAnimation from './detail-loading-animation/DetailLoadingAnimation';
 
-export { PersonAnimation, MiningLoadingAnimation };
+export { PersonAnimation, MiningLoadingAnimation, DetailLoadingAnimation };
