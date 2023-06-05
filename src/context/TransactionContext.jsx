dc7979cab909a5768d60da32618d3563edc12944
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-05 16:37:01 +0900

    transactionMined 코드 개선

--- src/context/TransactionContext.jsx
+++ src/context/TransactionContext.jsx
@@ -1,58 +1,51 @@
-import { createContext, useContext, useState } from 'react';
+import {createContext, useContext, useState} from 'react';
 
 const TransactionLoadingContext = createContext('');
 const TransactionParticipateContext = createContext('');
 
 export function useLoading() {
-  return useContext(TransactionLoadingContext);
+    return useContext(TransactionLoadingContext);
 }
 
 export function useParticipation() {
-  return useContext(TransactionParticipateContext);
+    return useContext(TransactionParticipateContext);
 }
 
 const isTransactionMined = async (
-  transactionHash,
-  provider,
-  changeLoadingStatus,
-  setTransactionSuccessModalTrue,
+    transactionHash,
+    provider,
+    changeLoadingStatus,
+    setTransactionSuccessModalTrue,
 ) => {
-  const txReceipt = await provider.getTransactionReceipt(transactionHash);
-  if (txReceipt && txReceipt.blockNumber) {
-    changeLoadingStatus(false);
-    setTransactionSuccessModalTrue && setTransactionSuccessModalTrue();
-  } else {
-    window.setTimeout(() => {
-      isTransactionMined(
-        transactionHash,
-        provider,
-        changeLoadingStatus,
-        setTransactionSuccessModalTrue,
-      );
-    }, 2500);
-  }
+    provider.waitForTransaction(transactionHash).then((receipt) => {
+        changeLoadingStatus(false);
+        setTransactionSuccessModalTrue && setTransactionSuccessModalTrue();
+    }).catch((error) => {
+        changeLoadingStatus(false);
+        console.alert('이더리움 네트워크에서 에러가 발생했습니다');
+    });
 };
 
-export const TransactionProvider = ({ children }) => {
-  const [loadingScreen, setLoadingScreen] = useState(false);
-  const [makeUnionDone, setMakeUnionDone] = useState(false);
-  const [participateDone, setParticipateDone] = useState(false);
+export const TransactionProvider = ({children}) => {
+    const [loadingScreen, setLoadingScreen] = useState(false);
+    const [makeUnionDone, setMakeUnionDone] = useState(false);
+    const [participateDone, setParticipateDone] = useState(false);
 
-  return (
-    <TransactionLoadingContext.Provider
-      value={{
-        isTransactionMined,
-        loadingScreen,
-        setLoadingScreen,
-        makeUnionDone,
-        setMakeUnionDone,
-      }}
-    >
-      <TransactionParticipateContext.Provider
-        value={{ participateDone, setParticipateDone }}
-      >
-        {children}
-      </TransactionParticipateContext.Provider>
-    </TransactionLoadingContext.Provider>
-  );
+    return (
+        <TransactionLoadingContext.Provider
+            value={{
+                isTransactionMined,
+                loadingScreen,
+                setLoadingScreen,
+                makeUnionDone,
+                setMakeUnionDone,
+            }}
+        >
+            <TransactionParticipateContext.Provider
+                value={{participateDone, setParticipateDone}}
+            >
+                {children}
+            </TransactionParticipateContext.Provider>
+        </TransactionLoadingContext.Provider>
+    );
 };
