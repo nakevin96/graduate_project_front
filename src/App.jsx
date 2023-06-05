699294ff9f876394642bb5de25411e56f97e8d7a
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-05 16:16:08 +0900

    lighthouse 참고하여 성능 올려보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { Routes, Route } from 'react-router';
-import {
-  TransactionProvider,
-  WalletProvider,
-  SwapProvider,
-  UnionProvider,
-} from './context';
-import { NotFound } from './pages/not-found';
-import { Home } from './pages/home';
-import { Swap } from './pages/swap';
-import { Union } from './pages/union';
-import { UnionDetail } from './pages/union/union-detail';
+import React from 'react';
+import {Routes, Route} from 'react-router';
+import {TransactionProvider, WalletProvider, SwapProvider, UnionProvider} from './context';
+import {NotFound} from './pages/not-found';
+import {Home} from './pages/home';
 
 const App = () => {
-  return (
-    <TransactionProvider>
-      <WalletProvider>
-        <SwapProvider>
-          <UnionProvider>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="*" element={<NotFound />} />
-              <Route path="/swap" element={<Swap />} />
-              <Route path="/union" element={<Union />} />
-              <Route path="/unionDetail" element={<UnionDetail />} />
-            </Routes>
-          </UnionProvider>
-        </SwapProvider>
-      </WalletProvider>
-    </TransactionProvider>
-  );
+    const Swap = React.lazy(() => import('./pages/swap').then(module => ({default: module.Swap})));
+    const Union = React.lazy(() => import('./pages/union').then(module => ({default: module.Union})));
+    const UnionDetail = React.lazy(() => import('./pages/union/union-detail').then(module => ({default: module.UnionDetail})));
+
+    return (
+        <TransactionProvider>
+            <WalletProvider>
+                <SwapProvider>
+                    <UnionProvider>
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="*" element={<NotFound/>}/>
+                            <Route path="/swap"
+                                   element={<React.Suspense fallback={<div>Loading...</div>}><Swap/></React.Suspense>}/>
+                            <Route path="/union" element={<React.Suspense
+                                fallback={<div>Loading...</div>}><Union/></React.Suspense>}/>
+                            <Route path="/unionDetail" element={<React.Suspense
+                                fallback={<div>Loading...</div>}><UnionDetail/></React.Suspense>}/>
+                        </Routes>
+                    </UnionProvider>
+                </SwapProvider>
+            </WalletProvider>
+        </TransactionProvider>
+    );
 };
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from '@honkhonk/vite-plugin-svgr';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+    plugins: [svgr(), react(), compression({algorithm: 'brotliCompress', ext: '.br'})],
+    build: {
+        rollupOptions: {
+            treeshake: true
+        }
+    }
 });
